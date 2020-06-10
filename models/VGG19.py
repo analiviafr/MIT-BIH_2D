ed72d1833e9762f562eb320e015a618e3e7a4581
Ana Lívia Franco <analivia@live.com>
2020-06-10 18:52:34 -0300

    Update VGG19.py

--- models/VGG19.py
+++ models/VGG19.py
@@ -11,17 +11,17 @@ from tensorflow.keras.applications.vgg19 import VGG19
 
 def vgg19_model(input_shape, n_classes, freezed_layers=5):
   input_tensor = Input(shape=input_shape)
-  vgg16 = VGG19(include_top=False,
+  vgg19 = VGG19(include_top=False,
                   weights='imagenet',
                   input_tensor=input_tensor)
 
   top = Sequential()
-  top.add(Flatten(input_shape=vgg16.output_shape[1:]))
+  top.add(Flatten(input_shape=vgg19.output_shape[1:]))
   top.add(Dense(256, activation='relu'))
   top.add(Dropout(0.5))
   top.add(Dense(n_classes, activation='softmax'))
   
-  model = Model(inputs=vgg16.input, outputs=top(vgg16.output))
+  model = Model(inputs=vgg19.input, outputs=top(vgg19.output))
   for layer in model.layers[:freezed_layers]:
     layer.trainable = False
 
