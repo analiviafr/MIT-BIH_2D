508d8e156101ecabbdd74d086d8a9a281e292bc4
Ana Lívia Franco <analivia@live.com>
2020-07-02 14:14:12 -0300

    Update split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -1,6 +1,6 @@
 
 import split_folders
 
-split_folders.ratio('/home/analiviafr/TCC/Experimentos/MITBIH_img', output="/home/analiviafr/TCC/Experimentos/", seed=1337, ratio=(.8, .2))
+split_folders.ratio('/home/analiviafr/TCC/Experimentos/images', output="/home/analiviafr/TCC/Experimentos/ecg_images", seed=1337, ratio=(.8, .2))
 #80% treinamento
 #20% teste
