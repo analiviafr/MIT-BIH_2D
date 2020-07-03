f92875b62045499ea81ffb3b9f9852c7e4ec65d4
Ana Lívia Franco <analivia@live.com>
2020-07-02 21:20:38 -0300

    Update Training_2D.py

--- Training_2D.py
+++ Training_2D.py
@@ -13,6 +13,7 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from sklearn.metrics import classification_report, multilabel_confusion_matrix, confusion_matrix,f1_score, accuracy_score, recall_score, precision_score
 from imblearn.metrics import specificity_score
+import matplotlib.pyplot as plt
 
 from VGG16 import vgg16_model
 
@@ -25,9 +26,9 @@ test_generator = ImageDataGenerator(rotation_range = 7, horizontal_flip = True,
                                     shear_range = 0.2, height_shift_range = 0.07,
                                     zoom_range = 0.2)
 
-train_data_generator = train_generator.flow_from_directory('/content/drive/My Drive/TCC_IC_2020/ecg_img/split10percent/train')
+train_data_generator = train_generator.flow_from_directory('/content/drive/My Drive/experimentos/ecg_img/train')
 
-test_data_generator = test_generator.flow_from_directory('/content/drive/My Drive/TCC_IC_2020/ecg_img/split10percent/test')
+test_data_generator = test_generator.flow_from_directory('/content/drive/My Drive/experimentos/ecg_img/test')
 
 model = vgg16_model(input_shape=(256, 256, 3), n_classes=5)
 opt = optimizers.Adam(lr = 0.05)
@@ -41,13 +42,11 @@ callbacks_list = [checkpoint, es, rlr]
 history = model.fit_generator(train_data_generator, epochs= 50, steps_per_epoch=(train_data_generator.samples/train_data_generator.batch_size), 
                                       validation_data = test_data_generator, validation_steps=(test_data_generator.samples/test_data_generator.batch_size))
 
-#model.load_weights('pesos.h5')
+model.load_weights('pesos.h5')
 
-#Confution Matrix and Classification Report
 test_steps_per_epoch = np.math.ceil(test_data_generator.samples/test_data_generator.batch_size)
 
 predictions = model.predict_generator(test_data_generator, steps=test_steps_per_epoch)
-# Get most likely class
 predicted_classes = np.argmax(predictions, axis=1)
 
 true_classes = test_data_generator.classes
@@ -64,22 +63,15 @@ print(accuracy_score(true_classes, predicted_classes))
 
 print('Sensibilidade')
 print(recall_score(true_classes, predicted_classes, average='micro'))
-print(recall_score(true_classes, predicted_classes, average='macro'))
 
 print('Precisão')
 print(precision_score(true_classes, predicted_classes, average='micro'))
-print(precision_score(true_classes, predicted_classes, average='macro'))
 
 print('Especificidade')
 print(specificity_score(true_classes, predicted_classes, average='micro'))
-print(specificity_score(true_classes, predicted_classes, average='macro'))
 
 print('F1-score')
 print(f1_score(true_classes, predicted_classes, average='micro')) 
-print(f1_score(true_classes, predicted_classes, average='macro')) 
-
-#GRÁFICO PARA COMPARAR RESULTADOS DO TESTE
-import matplotlib.pyplot as plt
 
 gof_train_acc  = history.history['accuracy']
 gof_train_loss = history.history['loss'    ]
